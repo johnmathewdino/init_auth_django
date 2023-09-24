d4ae4334864da4483036397b8a5524838675ed0b
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2023-09-24 19:08:50 +0800

    Fixed homepage
    can only be accessed if authenticated

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required(login_url='login')
 def homepage(request):
     return render(request, 'homepage.html')
