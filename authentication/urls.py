6294324a0d75c1283f5bc022afe9661b3502770a
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2023-09-24 18:58:08 +0800

    Add login and logout functionality

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,20 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from authentication import views
 
 urlpatterns = [
+    # login view from auth_views with custom login template
+    path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html',
+                                                # True means that if user is already logged in, it will redirect to homepage
+                                                redirect_authenticated_user=True), name='login'),
+
+    # logout view from auth_view
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # path for homepage where successfull login will redirect
+    path('', views.homepage, name='homepage'),
+
+
 
 
 ]
--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "homepage"
+LOGOUT_REDIRECT_URL = "login"
