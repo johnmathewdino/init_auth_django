f67e04053d0e64c995db5f67dc9e32f07eec6091
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2024-09-17 20:13:15 +0800

    Added auth form class
    Fix #4 #3

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,63 +1,97 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.forms import forms
 
 
 # uncomment this if you want to change the class/design of the login form
-# class UserLoginForm(AuthenticationForm):
-#     class Meta:
-#         model = User
-#         fields = ['username', 'password']
-#     def __init__(self, *args, **kwargs):
-#         super(UserLoginForm, self).__init__(*args, **kwargs)
-#         self.fields['username'].widget.attrs.update({
-#             'class': 'form-control',
-#             'placeholder': 'Username',
-#             'required': 'True'
-#         })
-#         self.fields['password'].widget.attrs.update({
-#             'class': 'form-control',
-#             'placeholder': 'Password',
-#             'required': 'True'
-#         })
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Username',
+            'required': 'True'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'required': 'True'
+        })
 
 
 # Customizing Registration Form from UserCreationForm
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1','password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
     # uncomment this if you want to change the class/design of the registration form inputs
-    # def __init__(self, *args, **kwargs):
-    #     super(UserRegistrationForm, self).__init__(*args, **kwargs)
-    #     self.fields['first_name'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'placeholder': 'First Name',
-    #         'required': 'True'
-    #     })
-    #     self.fields['last_name'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'placeholder': 'Last Name',
-    #         'required': 'True'
-    #     })
-    #     self.fields['username'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'placeholder': 'Username',
-    #         'required': 'True'
-    #     })
-    #     self.fields['email'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'placeholder': 'Email',
-    #         'required': 'True'
-    #     })
-    #     self.fields['password1'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'placeholder': 'Password',
-    #         'required': 'True'
-    #     })
-    #     self.fields['password2'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'placeholder': 'Retype Password',
-    #         'required': 'True'
-    #     })
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'First Name',
+            'required': 'True'
+        })
+        self.fields['last_name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Last Name',
+            'required': 'True'
+        })
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Username',
+            'required': 'True'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Email',
+            'required': 'True'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'required': 'True'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Retype Password',
+            'required': 'True'
+        })
+
+
+class ResetPasswordForm(PasswordResetForm):
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def __init__(self, *args, **kwargs):
+        super(ResetPasswordForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Email',
+            'required': 'True'
+        })
+
+
+class ResetPasswordConfirmForm(SetPasswordForm):
+    class Meta:
+        model = User
+        fields = ['new_password1', 'new_password2']
+
+    def __init__(self, *args, **kwargs):
+        super(ResetPasswordConfirmForm, self).__init__(*args, **kwargs)
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'New Password',
+            'required': 'True'
+        })
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Retype New Password',
+            'required': 'True'
+        })
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from authentication import views
+from authentication.forms import UserLoginForm, ResetPasswordConfirmForm, ResetPasswordForm
 
 urlpatterns = [
     # login view from auth_views with custom login template
     path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html',
+                                                form_class=UserLoginForm,
                                                 # True means that if user is already logged in, it will redirect to homepage
-                                                redirect_authenticated_user=True), name='login'),
+                                                redirect_authenticated_user=True), name='login',
+         ),
 
     # logout view from auth_view
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -18,13 +21,15 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 
     #path to reset password
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='authentication/password_reset.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='authentication/password_reset.html',
+                                                                 form_class=ResetPasswordForm), name='password_reset'),
 
     #path to password_reset_done
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_done.html'), name='password_reset_done'),
 
     #path to password_reset_confirm
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html',
+                                                                                                 form_class=ResetPasswordConfirmForm), name='password_reset_confirm'),
 
     #path to password reset complete
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
